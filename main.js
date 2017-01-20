14c77f259f293db553c06066510aaa61e1ae9e6b
Guilherme Pimentel <ggampp@gmail.com>
2017-01-19 22:30:07 -0200

    First

---
+++ main.js
@@ -0,0 +1,17 @@
+var http    = require('http');
+var url     = require('url');
+
+var dispatcher = require('./lib/dispatcher.js');
+
+http.createServer(function(req, res){
+    try{
+        console.log('Incoming Request from: ' + req.connection.remoteAddress + 'for href: ' + url.parse(req.url).href);
+        dispatcher.dispatch(req, res);
+    }catch(err){
+        console.log(err);
+        res.writeHead(500);
+        res.end('Internal Server Error');
+    }
+}).listen('1337','127.0.0.1', function(){
+    console.log('Server running at http://127.0.0.1:1337/');
+});
